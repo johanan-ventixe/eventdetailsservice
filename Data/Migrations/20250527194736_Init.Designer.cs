005f4b4ab4cea67cd9b39bcc533358ea679cd60d
Johan An <johanan9606@gmail.com>
2025-06-01 01:12:24 +0200

    deployed to azure web app

--- Data/Migrations/20250527194736_Init.Designer.cs
+++ Data/Migrations/20250527194736_Init.Designer.cs
@@ -11,7 +11,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Data.Migrations
 {
     [DbContext(typeof(DataContext))]
-    [Migration("20250522165029_Init")]
+    [Migration("20250527194736_Init")]
     partial class Init
     {
         /// <inheritdoc />
